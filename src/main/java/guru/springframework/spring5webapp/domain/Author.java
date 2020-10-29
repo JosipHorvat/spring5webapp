e3cdee3344fc3211da7d317dee2bcfd18033bddb
JosipHorvat <josiph988@gmail.com>
2020-10-29 22:11:15 +0100

    First 2 pojo classes, mapped @ManyToMany and id as PK

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,11 +1,22 @@
 package guru.springframework.spring5webapp.domain;
+/*
+Josip 29/10/2020
 
+ */
+import javax.persistence.*;
 import java.util.Set;
 
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authors")
     private Set<Book> books;
 
     public Author() {
@@ -15,6 +26,14 @@ public class Author {
         this.firstName = firstName;
         this.lastName = lastName;
         this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
     }
 
     public String getFirstName() {
